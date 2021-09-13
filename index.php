5f190b463143aa9d555b05cc446f11a8c90dc9fe
Justin2799 <45048865+Justin2799@users.noreply.github.com>
2021-09-13 14:24:59 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<?php
+  include_once 'header.php';
+?>
+
+<section class="index-intro">
+  <h1>Simple Scheduling</h1>
+  <p>Create an account to simplify your work day!</p>
+</section>
+
+<section class="index-categories">
+  <h2>Login or create an account here!</h2>
+  <div class="index-categories-list">
+    <div>
+		<a href="login.php"</a>
+      <h3>Login</h3>	
+    </div>
+    <div>
+		<a href="signup.php"</a>
+      <h3>Create an Account</h3>
+    </div>
+</section>
+
+<?php
+  include_once 'footer.php';
+?>
